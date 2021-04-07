c36e344b38580980dea2d517df76bd208dd497eb
Kenton Renshaw <kentorenshaw@gmail.com>
2021-04-07 00:10:41 -0600

    Challenge 4

---
+++ Prototype4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 8;
+    private GameObject focalPoint;
+    private GameObject boostParticles;
+
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 5;
+
+    private float normalStrength = -10; // how hard to hit enemy without powerup
+    private float powerupStrength = -25; // how hard to hit enemy with powerup
+    public float boostMultiplier = 2;
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+        boostParticles = GameObject.Find("Boost");
+        boostParticles.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Add force to player in direction of the focal point (and camera)
+        float verticalInput = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.Space))
+        {
+            boostParticles.SetActive(true);
+            playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * boostMultiplier);
+        }
+        else
+        {
+            boostParticles.SetActive(false);
+            playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed);
+        }
+
+        // Set powerup indicator position to beneath player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+        boostParticles.transform.position = transform.position;
+        boostParticles.transform.rotation = focalPoint.transform.rotation;
+    }
+
+    // If Player collides with powerup, activate powerup
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown());
+        }
+    }
+
+    // Coroutine to count down powerup duration
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    // If Player collides with enemy
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer =  transform.position - other.gameObject.transform.position; 
+           
+            if (hasPowerup) // if have powerup hit enemy with powerup force
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+            else // if no powerup, hit enemy with normal strength 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+        }
+    }
+}
